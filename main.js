3b572b7a4518f25d2eb9f9d44410b029b5c6fdeb
Jeremias <jeremias@camba.coop>
2018-08-24 17:59:34 -0300

    POE-1 Se agregaron los archivos necesarios para comenzar

---
+++ main.js
@@ -0,0 +1,9 @@
+let textBlock = document.getElementById('poem');
+let categorizeButton = document.getElementById('categorize');
+let categoryTitle = document.getElementById('category-title');
+
+categorizeButton.onclick = function() {
+
+  categoryTitle.textContent = 'La categoria es:';
+
+};
